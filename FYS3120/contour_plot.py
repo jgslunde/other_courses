21b39f69cc1b96b0b7fa5b658e4393eca7a5d429
asdfbat <jonas.s.lunde@outlook.com>
2018-03-21 09:11:03 -0700

    working on FYS3120 home exam

--- FYS3120/contour_plot.py
+++ FYS3120/contour_plot.py
@@ -1,24 +1,19 @@
 import matplotlib.pyplot as plt
 from numpy import meshgrid, pi, linspace, cos
 
-I = 1
-m = 1
-g = 9.81
-b = 1
+def H(phi, p, I=1, m=1, g=9.81, b=1):
+    return 0.5*p**2/(m*b**2 + I) - m*g*b*cos(phi) + m*g*b*lambd*phi
 
-p = linspace(-10, 10, 400)
-phi = linspace(0, 4*pi, 400)
+p = linspace(-20, 20, 400)
+phi = linspace(-2*pi, 2*pi, 400)
 
 p, phi = meshgrid(p, phi)
 
-
-
-for lambd in linspace(0,1.6,9):
-    H = 0.5*p**2/(m*b**2 + I) - m*g*b*cos(phi) + m*g*b*lambd*phi
-    plt.figure()
-    plt.contour(phi, p, H, 16)
+for lambd in linspace(0,1.05,8):
+    plt.figure(figsize=(5,3))
+    plt.contour(phi, p, H(phi, p), 16)
     plt.ylabel("$p'_\phi$", size=16)
     plt.xlabel("$\phi$", size=16)
-    plt.title("$\lambda=$%.1f" % lambd, size=16)
+    plt.title("$\lambda=$%.2f" % lambd, size=16)
     plt.tight_layout()
-    plt.savefig("fig/lambd=%.1f.pdf" % lambd)
+    plt.savefig("fig/lambd=%.2f.pdf" % lambd)
