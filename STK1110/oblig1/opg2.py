631daf0dfacdcf2089453f9c1fc4818595c289e8
asdfbat <jonas.s.lunde@outlook.com>
2018-09-29 06:29:24 -0700

    oblig1 stk1110

--- STK1110/oblig1/opg2.py
+++ STK1110/oblig1/opg2.py
@@ -2,33 +2,35 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.stats as st
 
-n = 8
-N = 1000
-mu = 1
+
+
+
+def get_hit_rate(n, N, alpha=0.05, mu=1, sigma=1):
+    data = np.random.normal(loc=mu, scale=sigma, size=(N, n))
+
+    alpha = 0.05
+    df = n - 1
+
+    t_ppf_lower = st.t.ppf(1 - alpha/2, df)
+    t_ppf_upper = st.t.ppf(alpha/2, df)
+
+    X_bar = np.mean(data, axis=1)
+    S = np.std(data, axis=1)
+
+    conf_int = np.zeros((N,2))
+    conf_int[:,0] = X_bar - t_ppf_lower*S/np.sqrt(n)
+    conf_int[:,1] = X_bar - t_ppf_upper*S/np.sqrt(n)
+
+    does_contain_mu = (conf_int[:,0] < mu) * (mu < conf_int[:,1])
+    hit_rate = np.sum(does_contain_mu)/N
+
+    return hit_rate
 
 
 # TASK 2c)
-data = np.random.normal(loc=mu, scale=1.0, size=(1000,8))
-
-alpha = 0.05
-df = n - 1
-
-t_ppf_lower = st.t.ppf(1 - alpha/2, df)
-t_ppf_upper = st.t.ppf(alpha/2, df)
-
-X_bar = np.mean(data, axis=1)
-S = np.std(data, axis=1)
-
-conf_int = np.zeros((N,2))
-conf_int[:,0] = X_bar - t_ppf_lower*S/np.sqrt(n)
-conf_int[:,1] = X_bar - t_ppf_upper*S/np.sqrt(n)
-
-does_contain_X = np.zeros(N)
-does_contain_X = np.greater(conf_int[:,1], X_bar) * np.greater(X_bar, conf_int[:,0])
-
-does_contain_mu = (conf_int[:,0] < mu) * (mu < conf_int[:,1])
-hit_rate = np.sum(does_contain_mu)/N
-print(hit_rate)
-
+print( get_hit_rate(n=8, N=1000) )
 
 # TASK 2d)
+print( get_hit_rate(n=30, N=1000) )
+print( get_hit_rate(n=200, N=1000) )
+
